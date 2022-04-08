d2c409a71857b283bf6430b342b4e5b3cefbeb87
patrickfenn <patrickrickenbachfenn@yahoo.com>
2022-04-08 13:19:18 -0700

    typo

--- bjpenn_parse.py
+++ bjpenn_parse.py
@@ -3,6 +3,5 @@ from bs4 import BeautifulSoup
 def parse():
     url = 'https://www.bjpenn.com'
     r = requests.get("https://www.bjpenn.com/mma-news/ufc/jairzinho-rozenstruik-warns-marcin-tybura-of-his-power-ahead-of-ufc-273-as-soon-as-i-start-touching-people-they-have-big-problems/")
-    soup - BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-    
