2570dc0ec1b2ca9b7620f37f1e253c07c83a1f15
patrickfenn <patrickrickenbachfenn@yahoo.com>
2022-04-08 15:54:21 -0700

    tweaked to use class structure

--- bjpenn_parse.py
+++ bjpenn_parse.py
@@ -1,7 +1,35 @@
 from bs4 import BeautifulSoup
+import requests
+import re
 
-def parse():
-    url = 'https://www.bjpenn.com'
-    r = requests.get("https://www.bjpenn.com/mma-news/ufc/jairzinho-rozenstruik-warns-marcin-tybura-of-his-power-ahead-of-ufc-273-as-soon-as-i-start-touching-people-they-have-big-problems/")
-    soup = BeautifulSoup(r.text, 'html.parser')
+class bjpenn_parse:
 
+    def parse(self):
+        url = 'https://www.bjpenn.com'
+        r = requests.get("https://www.bjpenn.com/mma-news/ufc/jairzinho-rozenstruik-warns-marcin-tybura-of-his-power-ahead-of-ufc-273-as-soon-as-i-start-touching-people-they-have-big-problems/")
+        soup = BeautifulSoup(r.text, 'html.parser')
+        return soup
+
+    #need to fix regex for http urls.
+    def parseLink(self, entry):
+        url = re.findAll("http.*/",entry);
+        print(url)
+
+    def getRelativeLinks(self, soup):
+            relative_links = soup.findAll('li')
+
+            return relative_links
+
+    ##dirty links are the entries inside the table. the links need to be parsed and stripped.
+    def __init__(self):
+         bs = self.parse()
+         dirty_links = self.getRelativeLinks(bs)
+        
+
+def main():
+   bjp = bjpenn_parse()
+   print("test")
+
+
+if __name__ == "__main__":
+    main()
