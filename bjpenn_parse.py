e0b4a9c69107c3782d3430730e60a9cc14932f05
patrickfenn <patrickrickenbachfenn@yahoo.com>
2022-04-09 18:31:28 -0700

    added recursive function for links, added conda command for needed libraries to run

--- bjpenn_parse.py
+++ bjpenn_parse.py
@@ -1,35 +1,49 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
-import re
+import validators 
 
-class bjpenn_parse:
+class bjpenn_crawler:
 
-    def get_seed(self):
-        url = 'https://www.bjpenn.com'
-        r = requests.get("https://www.bjpenn.com/mma-news/ufc/jairzinho-rozenstruik-warns-marcin-tybura-of-his-power-ahead-of-ufc-273-as-soon-as-i-start-touching-people-they-have-big-problems/")
+    def get_seed(self,link):
+        r = requests.get(link)
         soup = BeautifulSoup(r.text, 'html.parser')
         return soup
 
-
+    #gets all links on a page. grabs anythin with an <a href tag so some are garbage (like '#')
     def get_dirty_links(self, soup_obj):
         dirty_links = []
         unparsed_links = soup_obj.findAll('a')
         for link in unparsed_links:
-            if link.has_attr('href') :
+            if link.has_attr('href')  and validators.url(link['href']) :
                 dirty_links.append(link['href'])
 
         return dirty_links
 
-    
+    #need to fix infinite loop, i think the condition applies to each function call and it doesnt check the 'super' link_list
+    def recurse_get_dirty_links(self,soup_obj):
+        
+        link_list = []
+        dirty_links = self.get_dirty_links(soup_obj)
+        if len(dirty_links) == 0 or len(link_list) > 200:
+            return list
+
+        link_list.append(dirty_links)
+        for link in dirty_links:
+            link_list.append(self.recurse_get_dirty_links(self.get_seed(link)))
+
+
+        return link_list
+
     def __init__(self):
-         bs = self.get_seed()
-         links = self.get_dirty_links(bs)
+        
+
+         seed = self.get_seed("https://www.bjpenn.com/mma-news/")
+         links = self.recurse_get_dirty_links(seed)
          print(links)
         
 
 def main():
-   bjp = bjpenn_parse()
+   bjp = bjpenn_crawler()
 
 
 if __name__ == "__main__":
